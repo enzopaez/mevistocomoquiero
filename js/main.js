993800eefe9b960fb80d9ed3df38f188822fb55f
enzopaez <54156425+enzopaez@users.noreply.github.com>
2020-06-02 21:50:53 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(main);
+
+
+function main() {
+
+    var contador = 0;
+    $(".gato").click(function() {
+        $(".principal").slideToggle();
+
+    });
+
+    $(".prod").click(function() {
+        $(".sub").slideToggle();
+        contador++;
+
+    });
+
+    $(".gato").click(function() {
+
+        if (contador % 2 == 1) {
+            $(".principal").slideUp();
+            $(".sub").slideUp();
+            contador--;
+        }
+
+    });
+
+
+    $('.scrollTo').click(function() {
+        var getElem = (".mapa");
+        var targetDistance = 20;
+        console.log($("#map"));
+
+        if ($(getElem).length) {
+            var getOffset = $(getElem).offset().top;
+            $('html,body').animate({
+                scrollTop: getOffset - targetDistance
+            }, 500);
+        }
+        return false;
+    });
+
+
+}
